470c4d5d9b0ffc175a0318ca027c4e2285d35ae1
dcarrano <dcarrano@gmail.com>
2016-06-16 09:52:08 -0400

    created routes & test template for event index

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('events');
+  this.route('gifts');
+  this.route('recipients');
+  this.route('users');
 });
 
 export default Router;
