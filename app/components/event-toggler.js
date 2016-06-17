f94bbd20ee18d094bdab701fa7debd2984297104
dcarrano <dcarrano@gmail.com>
2016-06-17 15:44:53 -0400

    ember login... works???

--- app/components/event-toggler.js
+++ app/components/event-toggler.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     toggleShow() {
-      this.toggleProperty('isPeople')
+      this.toggleProperty('isPeople');
     }
   }
 });
