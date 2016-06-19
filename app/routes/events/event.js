5d24d32f455ee4d56d63510f63bc30ffb458d6be
dcarrano <dcarrano@gmail.com>
2016-06-19 19:12:30 -0400

    working on basic new and delete actions

--- app/routes/events/event.js
+++ app/routes/events/event.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('event', params.event_id);
+  },
+
+  actions: {
+    delete() {
+      let eventToDelete = this.modelFor(this.routeName);
+      eventToDelete.destroyRecord().then(() => {
+        this.transitionTo('events')
+      })
+    }
   }
 });
--- app/routes/gifts/gift.js
+++ app/routes/gifts/gift.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('gift', params.gift_id);
+  },
+  
+  actions: {
+    delete() {
+      let giftToDelete = this.modelFor(this.routeName);
+      giftToDelete.destroyRecord().then(() => {
+        this.transitionTo('gifts')
+      })
+    }
   }
 });
