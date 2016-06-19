f4c38b612a4c0181be599175eb6ee489a8ad0ba2
snlamm <shmuel.lamm@gmail.com>
2016-06-19 16:20:32 -0400

    add event new form and action

--- app/routes/events.js
+++ app/routes/events.js
@@ -1,7 +1,15 @@
+
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('event');
-  }
+  },
+  actions: {
+    save: function(event) {
+      event.save().then(this.transitionTo("events.event", event)
+    ).catch((reason) =>{
+      alert("Something went wrong. Please try again.")
+    })
+  }}
 });
