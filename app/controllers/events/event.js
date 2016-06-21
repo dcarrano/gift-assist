44a8c16b6c32cc081fd944184974c466588cbecc
snlamm <shmuel.lamm@gmail.com>
2016-06-21 11:04:51 -0400

    add ability to create gifts within an event

--- app/controllers/events/event.js
+++ app/controllers/events/event.js
@@ -5,9 +5,13 @@ export default Ember.Controller.extend({
   actions: {
     toggleShow() {
       this.toggleProperty('isPeople')
+      this.toggleProperty('peopleView')
     },
     eventAddPerson() {
       this.toggleProperty('isAddingPerson')
+    },
+    eventAddGift() {
+      this.toggleProperty('isAddingGift')
     },
     update: function(recipient) {
       let recipient_names = this.get('model.recipients').mapBy('name')
@@ -21,6 +25,27 @@ export default Ember.Controller.extend({
         temp_recipient.save();
       } else {
         alert("That name is already associated with this event")
+      }
+    },
+    giftUpdate: function(gift, recipient) {
+      let recipName = recipient.get("name")
+      let giftName = gift.get('name')
+      if (recipName === undefined || giftName === undefined || giftName === "") {
+        alert("gift name and person name are required")
+        this.toggleProperty('isAddingGift')
+      } else {
+        let recipCapName = recipName.split(" ").map(function(word) {return word.capitalize()}).join(" ")
+        recipient.set('name', recipCapName)
+        recipient.set('events', [this.get('model')])
+
+
+        gift.set('event', this.get('model'))
+        recipient.save().then((recipient) => {
+          gift.set('recipient', recipient)
+          gift.save().then((gift) => {
+            this.toggleProperty('isAddingGift')
+          })
+        })
       }
     }
   }
